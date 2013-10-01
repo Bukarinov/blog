f9a629776d576992395b28a93c90a42f73bdb2fb
Bukarinov <bukarinov@gmail.com>
2013-10-01 18:19:35 +0400

    Move requirejs config to separate file

--- public/js/app.js
+++ public/js/app.js
@@ -1,49 +1,20 @@
-requirejs.config({
-    baseUrl: '/js',
-    paths: {
-        'jquery': 'bower_components/jquery/jquery',
-        'underscore': 'bower_components/lodash/dist/lodash.underscore',
-        'backbone': 'bower_components/backbone/backbone',
-        'localstorage': 'bower_components/backbone.localStorage/backbone.localStorage',
-        'handlebars': 'bower_components/handlebars.js/dist/handlebars',
-        'hbs': 'bower_components/hbs/hbs',
-        'json2': 'bower_components/hbs/hbs/json2',
-        'i18nprecompile': 'bower_components/hbs/hbs/i18nprecompile'
-    },
-    shim: {
-        'backbone': {
-            deps: ['underscore', 'jquery'],
-            exports: 'Backbone'
-        },
-        'underscore': {
-            exports: '_'
-        },
-        'handlebars': {
-            exports: 'Handlebars'
-        }
-    },
-    hbs: {
-        disableI18n: true,
-        disableHelpers: true
-    }
-});
+require(['config'], function() {
+    require(['backbone', 'modules/posts/route', 'modules/posts/models/posts', 'modules/posts/models/post'], function(Backbone, postsRoute, Posts, Post) {
+        /*var posts = new Posts();
 
+         posts.create({
+         title: 'test1',
+         description: 'test1 desc'
+         });
+         posts.create({
+         title: 'test2',
+         description: 'test2 desc'
+         });
+         posts.create({
+         title: 'test3',
+         description: 'test3 desc'
+         });*/
 
-requirejs(['backbone', 'modules/posts/route', 'modules/posts/models/posts', 'modules/posts/models/post'], function(Backbone, postsRoute, Posts, Post) {
-    /*var posts = new Posts();
-
-    posts.create({
-        title: 'test1',
-        description: 'test1 desc'
+        Backbone.history.start({pushState: true});
     });
-    posts.create({
-        title: 'test2',
-        description: 'test2 desc'
-    });
-    posts.create({
-        title: 'test3',
-        description: 'test3 desc'
-    });*/
-
-    Backbone.history.start({pushState: true});
 });
