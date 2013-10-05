1eadbea4ec1c9cd1dfd4890f51f1c50add6e02c2
Bukarinov <bukarinov@gmail.com>
2013-10-05 16:29:58 +0400

    Remove empty view

--- public/js/modules/posts/views/list.js
+++ public/js/modules/posts/views/list.js
@@ -1,17 +1,10 @@
-define(['backbone', 'marionette', 'modules/posts/models/post', 'modules/posts/views/post', 'hbs!modules/posts/templates/empty'],
-function(Backbone, Marionette, Post, PostView, EmptyTpl) {
+define(['backbone', 'marionette', 'modules/posts/models/post', 'modules/posts/views/post'],
+function(Backbone, Marionette, Post, PostView) {
     return Backbone.Marionette.CollectionView.extend({
         tagName: 'ul',
         itemView: PostView,
-
         events: {
             'click .delete': 'deletePost'
-        },
-
-        emptyView: function() {
-            return Backbone.Marionette.ItemView.extend({
-                template: EmptyTpl
-            });
         },
 
         deletePost: function(e) {
