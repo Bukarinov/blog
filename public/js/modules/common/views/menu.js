5f23abb12d6da1b777f7ef0d9d51089ed4a4575f
Bukarinov <bukarinov@gmail.com>
2013-10-09 22:06:48 +0400

    Add event for menu links

--- public/js/modules/common/views/menu.js
+++ public/js/modules/common/views/menu.js
@@ -1,6 +1,15 @@
 define(['backbone', 'marionette', 'hbs!modules/common/templates/menu'],
 function(Backbone, Marionette, menuTpl) {
     return Backbone.Marionette.ItemView.extend({
-        template: menuTpl
+        template: menuTpl,
+        events: {
+            'click menu a': '_clickMenuCallback'
+        },
+
+        _clickMenuCallback: function(e) {
+            e.preventDefault();
+
+            Backbone.history.navigate($(e.currentTarget).attr('href'), {trigger: true});
+        }
     });
 });
