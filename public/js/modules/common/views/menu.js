40fe102ddd641f49021092624d341cea871437b5
Bukarinov <bukarinov@gmail.com>
2013-10-13 22:25:37 +0400

    Add active class to menu

--- public/js/modules/common/views/menu.js
+++ public/js/modules/common/views/menu.js
@@ -2,6 +2,10 @@ define(['backbone', 'marionette', 'hbs!modules/common/templates/menu', 'styl!mod
 function(Backbone, Marionette, menuTpl) {
     return Backbone.Marionette.ItemView.extend({
         template: menuTpl,
+        ui: {
+            menu: 'menu',
+            links: 'a'
+        },
         events: {
             'click a': '_onClickLink'
         },
@@ -10,6 +14,9 @@ function(Backbone, Marionette, menuTpl) {
             e.preventDefault();
 
             Backbone.history.navigate(this.$(e.currentTarget).attr('href'), {trigger: true});
+
+            this.ui.links.removeClass('active');
+            this.ui.menu.find('a[href="/' + Backbone.history.fragment + '"]').addClass('active');
         }
     });
 });
